6d5fbdb8a877e0868d05160bc0764d3b8dd2a999
Sayed Golam Rasul Riaydh <sayed.riaydh@circlefintech.com>
2018-04-17 17:59:20 +0600

    first

---
+++ src/Page1.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-ons-page>
+    <custom-toolbar :back-label="'Page 2'">Page 2</custom-toolbar>
+    <keyboard
+            v-model="input"
+            @custom="custom"
+            @input="changed"
+            :layouts="[
+		'1234567890{delete:backspace}|qwertyuiop|asdfghjkl|{shift:goto:1}zxcvbnm|{space:space}{custom:custom}',
+		'!@#$%^&*(){delete:backspace}|QWERTYUIOP|ASDFGHJKL|{shift:goto:0}ZXCVBNM|{space:space}{custom:custom}'
+	]"
+            :maxlength="16"
+    ></keyboard>
+  </v-ons-page>
+</template>
+
+<script>
+  import customToolbar from './CustomToolbar';
+  import keyboard from 'vue-keyboard';
+  export default {
+    data() {
+      return {
+        input: ''
+      }
+    },
+    methods: {
+      changed(value) {
+        console.log('Value ' + value);
+      },
+
+      custom(keyboard) {
+        console.log(keyboard.value);
+      },
+      pop(){
+        this.pageStack.pop();
+      }
+     },
+     props: ['pageStack'],
+     components: { customToolbar,keyboard }
+  }
+</script>
---
+++ src/Page2.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-ons-page>
+    <custom-toolbar>Page 1</custom-toolbar>
+    <p style="text-align: center">
+      This is the first page
+      <v-ons-button @click="push">Push Page 2</v-ons-button>
+    </p>
+    <h1 class="text-center">Demo of vue-touch-keyboard</h1>
+    <div class="container" id="app">
+
+      <fieldset>
+        <legend>Normal layout</legend>
+        <input type="text" placeholder="Text input" @focus="show" data-layout="normal" />
+      </fieldset>
+
+      <fieldset>
+        <legend>Compact layout</legend>
+        <input type="text" placeholder="Text input" @focus="show" data-layout="compact" />
+      </fieldset>
+
+      <fieldset>
+        <legend>Numeric layout</legend>
+        <input type="text" placeholder="Text input" @focus="show" data-layout="numeric" />
+      </fieldset>
+
+      <vue-touch-keyboard id="keyboard" v-if="visible" :layout="layout" :cancel="hide" :accept="accept" :input="input" :next="next" />
+
+    </div>
+  </v-ons-page>
+</template>
+
+<script>
+  import Vue from 'vue';
+  import customToolbar from './CustomToolbar';
+  import page1 from './Page1';
+  import VueTouchKeyboard from "vue-touch-keyboard";
+  import style from "vue-touch-keyboard/dist/vue-touch-keyboard.css"; // load default style
+
+  Vue.use(VueTouchKeyboard);
+
+  export default {
+    data(){
+      return {
+        visible: false,
+        layout: "normal",
+        input: null,
+        options: {
+          useKbEvents: false
+         }
+        }
+      },
+    methods: {
+
+      push() {
+        this.pageStack.push(page1);
+      },
+      pop() {
+        this.pageStack.pop();
+      },
+      accept(text) {
+        console.log("Input text: " + text);
+        this.hide();
+      },
+
+      show(e) {
+        this.input = e.target;
+        this.layout = e.target.dataset.layout;
+
+        if (!this.visible)
+          this.visible = true
+      },
+
+      hide() {
+        this.visible = false;
+      },
+
+      next() {
+        let inputs = document.querySelectorAll("input");
+        let found = false;
+        [].forEach.call(inputs, (item, i) => {
+          if (!found && item == this.input && i < inputs.length - 1) {
+            found = true;
+            this.$nextTick(() => {
+              inputs[i+1].focus();
+            });
+          }
+        });
+        if (!found) {
+          this.input.blur();
+          this.hide();
+        }
+      }
+    },
+    props: ['pageStack'],
+    components: { customToolbar }
+  }
+</script>
